8cfed88f43f34923ec4c4ff672c37f25f948d3cc
Christopher <96350480+Ghoul0101@users.noreply.github.com>
2023-05-24 23:36:03 +0800

    Add files via upload

---
+++ loginPage.php
@@ -0,0 +1,25 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+	<link rel="stylesheet" href="StyleLogandSign.css">
+</head>
+<body>
+<div class="container">
+    <h1>Login</h1>
+    <form>
+      <input type="text" placeholder="Username">
+      <input type="password" placeholder="Password">
+      <button type="submit">Log in</button>
+	  <a href="SignupPage.php">Signup</a>
+    </form>
+</div>
+</body>
+</html>
