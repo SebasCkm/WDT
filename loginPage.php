f6215b1445f5bf7db861246998cac98ac4ae9144
Christopher <96350480+Ghoul0101@users.noreply.github.com>
2023-05-25 14:03:15 +0800

    Update loginPage.php

--- loginPage.php
+++ loginPage.php
@@ -10,6 +10,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login Page</title>
 	<link rel="stylesheet" href="StyleLogandSign.css">
+	<style>
+	.button {
+    display: inline-block;
+    padding: 10px 20px;
+    background-color: white;
+    color: lightslategray;
+    text-decoration: none;
+    border: none;
+    border-radius: 4px;
+    font-size: 16px;
+    cursor: pointer;
+  }
+  
+	.button:hover {
+    background-color: #ddd;
+  }
+  	</style>
 </head>
 <body>
 <div class="container">
@@ -18,7 +35,7 @@
       <input type="text" placeholder="Username">
       <input type="password" placeholder="Password">
       <button type="submit">Log in</button>
-	  <a href="SignupPage.php">Signup</a>
+	<a href="SignupPage.php" class="button">Signup</a>
     </form>
 </div>
 </body>
