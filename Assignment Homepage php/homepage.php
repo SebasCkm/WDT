049056d764dbf4f284470c3aee1c7569999da730
yxsh <113192973+Octaneman231@users.noreply.github.com>
2023-06-08 18:40:17 +0800

    Add files via upload

---
+++ Assignment Homepage php/homepage.php
@@ -0,0 +1,48 @@
+<?php
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+    <div class="container">
+        <div id="menu">
+            <div id="nav"> 
+                <img src="images/oldman.png" class="logo">
+                <ul>
+                    <li> <a style="text-decoration: none" href="services.php" >Login </a></li>
+                </ul>
+                <ul>
+                    <li> <a  style="text-decoration: none" href="homepage.php" Home >Home</a></li>
+                    <li>
+                        About us
+                        <ul>
+                            <li> <a style="text-decoration: none" href="portfolio.php" >Portfolio </a> </li>
+                            <li> <a style="text-decoration: none" href="our organization.php" >Our Organization </a></li>
+                            <li> <a style="text-decoration: none" href="our cause.html" >Our Cause </a></li>
+                        </ul>
+                    </li>
+                    <li> <a style="text-decoration: none" href="services.php" >Services </a></li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col">
+                <h1>Old People</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem incidunt, officia dolores at perferendis sapiente, adipisci iure veniam consequuntur fugiat architecto tempora quo, aliquam doloremque repudiandae distinctio neque reiciendis esse.</p>
+            </div>
+            <div class="pic pic1">
+                <h5>About Us</h5>
+                <p>We love old people Lorem ipsum dolor sit, amet consectetur adipisicing elit. Omnis necessitatibus beatae distinctio dolores odit harum architecto nostrum libero nisi repellendus expedita quasi, inventore quod amet fugit voluptatum dicta velit quam.</p>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
