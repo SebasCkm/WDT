da0ba6be1eb3b5cb700a0d711facf939c503fb66
SebasCkm <134512218+SebasCkm@users.noreply.github.com>
2023-06-19 18:07:09 +0800

    Add files via upload

---
+++ Assignment Service Page and Admin Page/RegisteredServicePage/RAccomodationPage.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="AccomodationPageStyles.css">
+</head>
+<body>
+<div class="container">
+        <div id="menu">
+            <div id="nav"> 
+                <img src="oldman.png" class="logo">
+                <ul>
+                    <li> <a style="text-decoration: none" href="services.php" >Logout </a></li>
+                    <li> <a style="text-decoration: none" href="services.php" >Enroll </a></li>
+                </ul>
+                <ul>
+                    <li> <a  style="text-decoration: none" href="homepage.php" Home >Home</a></li>
+                    <li>
+                        About us
+                        <ul>
+                            <li> <a style="text-decoration: none" href="portfolio.php" >Portfolio </a> </li>
+                            <li> <a style="text-decoration: none" href="our organization.php" >Our Organization </a></li>
+                            <li> <a style="text-decoration: none" href="our cause.html" >Our Cause </a></li>
+                        </ul>
+                    </li>
+                    <li> <a style="text-decoration: none" href="RServicePage.php" >Services </a></li>
+                </ul>
+            </div>
+        </div>
+        <h1 align="center">Accomodations Page</h1>
+    <main>
+    <div id="Accomodation flavor" class="info-area">
+        <h2>Our Accomodations</h2>
+     <table id="Accomodation Page Table" cellspacing="10">
+        <tr>
+         <td><h3>A Safe Space for One to Relax and Feel at Home</h3></td>
+     </tr>
+        <tr>
+          <td><p> To live alone is unpleasant. Therefor we offer accomodation that lets the elderly be able to live together in thier own safe space. <br>
+          Not only that, we know that some elderly prefer to be alone, we offer accomodation for that too. <br>
+          To give the elderly a place to call home, a place where they can relax and know that they have a safe place to be. <br> 
+           <br>
+           <br> Below are the accomodations that we offer.
+       </p></td>
+     </tr>
+     </table>
+     </div>
+     <table id="Squareinfoareatable" cellspacing="5">
+  <tr>
+    <td>
+      <div id="Squareinfo-area" class="info-area">
+      <h2>Accomodation type A</h2>
+      <p>In accomodation type A, it is an apartment with three rooms and a common area, kitchen and two toilets. <br> 
+    If you choose Accomodation type A, the elderly will need to share the apartment with other elderly.<br>
+    Below is a picture of the apartment.</p>
+    <img src="Apartment A 1.jpg" class="AccomodationPic">
+    
+
+      </div>
+    </td>
+
+    <table id="Squareinfoareatable" cellspacing="5">
+  <tr>
+    <td>
+      <div id="Squareinfo-area" class="info-area">
+      <h2>Accomodation type B</h2>
+      <p>In accomodation type B, it is a studio apartment with a kitchen and one toilet. <br> 
+    If you choose Accomodation type B, the elderly will not need to share the apartment with other elderly.<br>
+    Below is a picture of the apartment.</p>
+    <img src="Apartment B 1.jpg" class="AccomodationPic">
+    
+
+      </div>
+    </td>
---
+++ Assignment Service Page and Admin Page/RegisteredServicePage/RServicePage.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="SPageStyles.css">
+</head>
+<body>
+    <div class="container">
+        <div id="menu">
+            <div id="nav"> 
+                <img src="oldman.png" class="logo">
+                <ul>
+                    <li> <a style="text-decoration: none" href="services.php" >Logout </a></li>
+                    <li> <a style="text-decoration: none" href="services.php" >Enroll </a></li>
+                </ul>
+                <ul>
+                    <li> <a  style="text-decoration: none" href="homepage.php" Home >Home</a></li>
+                    <li>
+                        About us
+                        <ul>
+                            <li> <a style="text-decoration: none" href="portfolio.php" >Portfolio </a> </li>
+                            <li> <a style="text-decoration: none" href="our organization.php" >Our Organization </a></li>
+                            <li> <a style="text-decoration: none" href="our cause.html" >Our Cause </a></li>
+                        </ul>
+                    </li>
+                    <li> <a style="text-decoration: none" href="RServicePage.php" >Services </a></li>
+                </ul>
+            </div>
+        </div>
+        <h1 align="center">Service Page</h1>
+<main>
+<div id="Accommodation tab" class="info-area">
+  <h2>Accommodation</h2>
+    <table id="Accommodation Table" cellspacing="10">
+    <tr>
+      <td><h3>Confortable and Safe Spaces</h3></td>
+    </tr>
+    <tr>
+      <td><p>We offer accommodation that is confortable and with the staff and other residences around, you won't feel out of place.</p></td>
+    </tr>
+    </table>
+    <a href="RAccomodationPage.php" class="link-style" id="AccommodationLink" >Go to Accommodation Page</a>
+</div>
+
+    <div id = "Staff tab" class="info-area">
+      <h2>Staff</h2>
+      <table id="Staff Table" cellspacing="10">
+    <tr>
+      <td><h3>Kind, Caring and Loving</h3></td>
+    </tr>
+    <tr>
+      <td><p>With the help of our friendly and qualified staff, the elderly won't have any problems facing everyday.<br> They'll keep them company, safe and happy.</p></td>
+    </tr>
+    </table> 
+    <a href="RStaffPage.php" class="link-style" id="StaffLink">Go to Staff Page</a> 
+    </div>
+
+    <div  id = "Activites tab" class="info-area">
+      <h2>Activities</h2>
+      <table id="Activities Table" cellspacing="10">
+    <tr>
+      <td><h3>Fun, Interactive and Good for Cognative Health</h3></td> 
+    </tr>
+    <tr>
+      <td><p>Twice every week, the Staff will be holding activities that are fun for the elderly.<br>Not only that, the activities are helpful in keeping both the mind and body sharp and healthy.</p></td>
+    </tr>
+    </table> 
+    <a href="RActivitiesPage.php" class="link-style" id="ActivitiesLink">Go to Activities Page</a>
+    </div>
+  </main>
+    </div>
+</body>
+</html>
