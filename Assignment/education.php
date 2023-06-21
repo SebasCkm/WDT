a52f7acf574d8f1bef990a76c336837c07d62a09
yxsh <113192973+Octaneman231@users.noreply.github.com>
2023-06-21 16:01:44 +0800

    Add files via upload

---
+++ Assignment/education.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>About me</title>
+    <link rel="stylesheet" href="styles2.css">
+</head>
+<body>
+    <div class="container">
+        <div id="menu">
+            <div id="nav"> 
+                <img src="images/oldman.png" class="logo">
+                <ul>
+                    <li> <a  style="text-decoration: none" href="homepage.php" Home >Home</a></li>
+                        </ul>
+            </div>
+        </div>
+        <div class="main">
+            <div class="img">
+                <img src="images/yashmu.jpg" alt="">
+            </div>
+        <div class="text">
+            <h1>Education</h1>
+            <p>I am currently doing my diploma in APU. I did my IGSe O levels in Iskandhar School which is in my home country, Maldives.</p>
+            <div class="button">
+                <a href="aboutyashmu.php" class="bnt bnt1">Back</a>
+            </div>
+            
+        </div>
+</body>
+</html>
---
+++ Assignment/registeredusers.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styles2.css">
+</head>
+<body>
+    <div class="container">
+        <div id="menu">
+            <div id="nav"> 
+                <img src="images/oldman.png" class="logo">
+                <ul>
+                    <li> <a style="text-decoration: none" href="homepage.php" Home >Logout </a></li>
+            </div>
+        </div>
+</body>
+</html>
